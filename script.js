e870714797a021828ac5f6bee9010e907c2ca861
Aliaksa <sashvero@gmail.com>
2024-06-07 13:09:53 +0300

    feat: make it take only digits

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let result = document.getElementById("result");
 let number = +prompt("Enter number", "1");
 function toBinary(n) {
   let r;
-  if (typeof n == "number") {
+  if (isNaN(n)) {
+    return toBinary(+prompt("Enter only digits", "1"));
+  } else if (typeof n == "number") {
     r = +n.toString(2);
   }
   result.innerHTML = r;
